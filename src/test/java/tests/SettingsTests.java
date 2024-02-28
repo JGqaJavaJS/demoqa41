1be041dfe436aa98cea3450393eb44dde5b543d1
julia <juliiagordin@gmail.com>
2024-02-28 21:39:42 +0300

    add comments

--- src/test/java/tests/SettingsTests.java
+++ src/test/java/tests/SettingsTests.java
@@ -11,6 +11,7 @@ public class SettingsTests extends BaseTest{
 
 @Test
     public void test_001(){
+    //hard coding  with the folder tools and driver chrome
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://demoqa.com/");
         try {
@@ -23,6 +24,8 @@ public class SettingsTests extends BaseTest{
 
     @Test
     public void test_002() {
+
+    // hard coding to check webdrivermanager
         ChromeOptions chromeOptions = new ChromeOptions();
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver(chromeOptions);
@@ -48,6 +51,7 @@ public class SettingsTests extends BaseTest{
 
     @Test
     public void test_004() {
+    // here we extends from BaseTest and run chrome from ConfigManager
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
